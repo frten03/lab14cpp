84f1bc2c368cc953e7e890f99ca5fc9a5d8a8e19
Tenten <pakpichamoon@gmail.com>
2025-01-28 00:15:52 +0700

    finish

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-using namespace std;
-
 template <typename T>
 void insertionSort(T d[],int N){
 	for(int i=1;i<N;i++){
@@ -20,18 +17,3 @@ void insertionSort(T d[],int N){
 
 	}
 }
-
-
-
-int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
-	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
-	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
-	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
-}
-
